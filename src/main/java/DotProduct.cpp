05313d7e8a56f3aa02106f787cd924ba132cf06d
kcieslik226138 <kcieslik226138@e-science.pl>
2018-05-23 00:29:21 +0200

    Adding dotProduct class.

--- src/main/java/DotProduct.cpp
+++ src/main/java/DotProduct.cpp
@@ -1,11 +1,11 @@
 #include <jni.h>
 #include <iostream>
-#include "HelloJNI.h"
+#include "DotProduct.h"
 using namespace std;
 
 // Implementation of native method sayHello() of HelloJNI class
-JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject thisObj) {
-    jclass hello_world_class = env->FindClass("HelloJNI");
+JNIEXPORT void JNICALL Java_DotProduct_sayHello(JNIEnv *env, jobject thisObj) {
+    jclass hello_world_class = env->FindClass("DotProduct");
 
     jmethodID print_dupa_method;
     print_dupa_method = env->GetStaticMethodID(hello_world_class, "printDupa","()V");
